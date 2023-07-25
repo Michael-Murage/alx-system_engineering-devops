69542b494539bd3f8174127214604aa758977a39
Michael <mickeymurage@gmail.com>
2023-07-25 10:37:06 +0300

    fixed possible bug

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,18 +7,19 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
-        argv[1])
-    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    if argv[1]:
+        todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+            argv[1])
+        name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+        todo_result = get(todo_url).json()
+        name_result = get(name_url).json()
 
-    todo_list = []
-    for todo in todo_result:
-        todo_dict = {}
-        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
-            "completed"), "username": name_result.get("username")})
-        todo_list.append(todo_dict)
+        todo_list = []
+        for todo in todo_result:
+            todo_dict = {}
+            todo_dict.update({"task": todo.get("title"), "completed": todo.get(
+                "completed"), "username": name_result.get("username")})
+            todo_list.append(todo_dict)
 
-    with open("{}.json".format(argv[1]), 'w') as f:
-        dump({argv[1]: todo_list}, f)
+        with open("{}.json".format(argv[1]), 'w') as f:
+            dump({argv[1]: todo_list}, f)
